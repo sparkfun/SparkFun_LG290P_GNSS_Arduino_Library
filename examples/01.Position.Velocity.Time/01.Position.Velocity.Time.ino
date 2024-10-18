7ff047d738f4bb6a81ddfeae90bad7bba7b51454
Nathan Seidle <nathan@sparkfun.com>
2024-10-18 12:20:03 -0600

    Ex1: Turn off debug. Short delay. Use SIV.

--- examples/01.Position.Velocity.Time/01.Position.Velocity.Time.ino
+++ examples/01.Position.Velocity.Time/01.Position.Velocity.Time.ino
@@ -33,20 +33,25 @@ int gnss_baud = 460800;
 LG290P myGNSS;
 HardwareSerial SerialGNSS(1); // Use UART1 on the ESP32
 
+unsigned long lastCheck = 0;
+
 void setup()
 {
   Serial.begin(115200);
-  delay(3000);
+  delay(250);
   Serial.println();
+
   Serial.println("SparkFun LG290P Position, Velocity, Time example");
   Serial.println("Initializing device...");
+
   // We must start the serial port before using it in the library
   // Increase buffer size to handle high baud rate streams
   SerialGNSS.setRxBufferSize(1024);
   SerialGNSS.begin(gnss_baud, SERIAL_8N1, pin_UART1_RX, pin_UART1_TX);
-  
-  myGNSS.enableDebugging(Serial); // Print all debug to Serial
-  if (!myGNSS.begin(SerialGNSS))     // Give the serial port over to the library
+
+  //myGNSS.enableDebugging(Serial); // Print all debug to Serial
+
+  if (myGNSS.begin(SerialGNSS) == false)     // Give the serial port over to the library
   {
     Serial.println("LG290P failed to respond. Check ports and baud rates. Freezing...");
     while (true);
@@ -58,20 +63,19 @@ void loop()
 {
   myGNSS.update(); // Regularly call to parse any new data
 
-  static unsigned long lastCheck = 0;
   if (millis() - lastCheck > 1000)
   {
     lastCheck = millis();
 
-    // The get methods are updated whenever new data is parsed with the update() call.
+    // The 'get' methods are updated whenever new data is parsed with the update() call.
     // By default, this data is updated once per second.
     Serial.printf("Lat/Long/Alt: %.8f/%.8f/%.2f\r\n", myGNSS.getLatitude(), myGNSS.getLongitude(), myGNSS.getAltitude());
-    Serial.printf("Horizontal Speed: %.2fm/s Course: %.2f degrees\r\n", 
-      myGNSS.getHorizontalSpeed(), myGNSS.getCourse());
+    Serial.printf("Horizontal Speed: %.2fm/s Course: %.2f degrees\r\n",
+                  myGNSS.getHorizontalSpeed(), myGNSS.getCourse());
     Serial.printf("Date (yyyy/mm/dd): %04d/%02d/%02d Time (hh:mm:ss) %02d:%02d:%02d.%03d\r\n",
-      myGNSS.getYear(), myGNSS.getMonth(), myGNSS.getDay(), 
-      myGNSS.getHour(), myGNSS.getMinute(), myGNSS.getSecond(), myGNSS.getMillisecond());
-    Serial.printf("Satellites used: %d\r\n", myGNSS.getSatellitesUsed());
+                  myGNSS.getYear(), myGNSS.getMonth(), myGNSS.getDay(),
+                  myGNSS.getHour(), myGNSS.getMinute(), myGNSS.getSecond(), myGNSS.getMillisecond());
+    Serial.printf("Satellites in view: %d\r\n", myGNSS.getSatellitesInView());
     Serial.println();
   }
 }
